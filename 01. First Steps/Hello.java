ce45de6f27313df761bd4bdc176e871b986ce55d
Navdeep Sharma <navdeepsharmatu@gmail.com>
2022-10-07 16:30:12 +0530

    hello world

---
+++ 01. First Steps/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    // public is an access modifier
+    // void indicates method won't return anything
+    // method contains a set of instructions
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+// Hello World
