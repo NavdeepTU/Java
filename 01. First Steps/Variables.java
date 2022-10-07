00168465d05d40bee8375b5c7b31f7bb59f95a80
Navdeep Sharma <navdeepsharmatu@gmail.com>
2022-10-07 17:03:43 +0530

    Variables updated

--- 01. First Steps/Variables.java
+++ 01. First Steps/Variables.java
@@ -1,7 +1,15 @@
 class Variables {
     public static void main(String[] args) {
+
         int myFirstNumber = (10 + 5) + (2 * 10); // dataType variableName assigned_value(optional)
-        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
 
