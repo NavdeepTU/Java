7b22380d97d6d5ede8281c18b3f0588817c9bcce
Navdeep Sharma <navdeepsharmatu@gmail.com>
2023-01-29 11:45:09 +0530

    added hello world code

---
+++ 01. Introduction to Java Language/01. FirstClass.java
@@ -0,0 +1,20 @@
+/*
+ * 	JDK(Java Development Kit) -> JRE(Java Runtime Environment) -> JVM(Java Virtual Machine)
+ *	Compilation :
+ *		- Source code (.java file) is converted (by JDK) to Byte Code (.class file)
+ *		- Byte Code is machine independent.
+ *		- Byte Code can run on any machine as long as (the machine) has JRE.
+ *	Execution :
+ *		- Byte Code is converted into machine's native language (eg. binary) by JVM.
+ */
+
+
+class FirstClass {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
+}
+/*
+ *	Hello World
+ */
+
