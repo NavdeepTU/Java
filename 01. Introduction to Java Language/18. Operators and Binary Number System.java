409c9d3e263bf48cb4bf14598187f76900005d31
Navdeep Sharma <navdeepsharmatu@gmail.com>
2023-02-09 20:09:59 +0530

    added operators and binary number system code

---
+++ 01. Introduction to Java Language/18. Operators and Binary Number System.java
@@ -0,0 +1,45 @@
+// Binary Operators(works with two operands)          Unary Operators(works with only one operand)
+// +                                                  ++ (increment)
+// -                                                  ++a -> pre increment -> first increment then use
+// *                                                  a++ -> post increment -> first use then incement
+// /                                                  -- (decrement)
+// %                                                  --a -> pre decrement -> first decrement then use
+//                                                    a-- -> post decrement -> first use then decrement
+
+
+// Relational operators
+// ==
+// !=
+// >
+// <
+// >=
+// <=
+
+// Logical Operators
+// &&
+// || -> will do computation until it gets first true statement or reaches end of the expression
+// !
+
+// Binary Number System (Base 2)
+// Binary to Decimal conversion -> Multiply bit with 2 to the power of
+// Decimal to Binary conversion -> Using LCM method
+
+// Octal (Base 8)
+
+// Hexadecimal(Base 16) -> Memory addresses Hexadecimal mai store hote hai
+
+/* Bitwise Operators
+ * &  (Binary AND)
+ * |  (Binary OR)
+ * ^  (Binary XOR)
+ * ~  (Binary One's Complement)
+ * << (Binary Left Shift) -> number << # of positions you want to left shift bits
+ * >> (Binary Right Shift) -> number << # of positions you want to right shift bits
+ */
+ 
+ // Assignment Operators
+ // =
+ // +=
+ // -=
+ // *=
+ // /=
