a5d5ac2986537e6d77ca046a5a761e63b9d02c38
Navdeep Sharma <navdeepsharmatu@gmail.com>
2023-02-27 20:02:36 +0530

    added basic oops, polymorphism and Inheritance code

---
+++ 03. Object Oriented Programming/Inheritance.java
@@ -0,0 +1,39 @@
+class Shape {
+	String color;
+	
+	public void area() {
+		System.out.println("displays area");
+	}
+}
+
+// Single level Inheritance
+class Triangle extends Shape {  // Triangle class will have all the properties and methods that Shape class has -> this is known as Inheritance
+	public void area(int l, int h) {
+		System.out.println(1/2*l*h);
+	}
+}
+
+// Multilevel Inheritance
+class EquilateralTriangle extends Triangle { // EquilateralTriangle class will have all the properties and methods that Triangle and Shape class has
+	public void area(int l, int h) {
+		System.out.println(1/2*l*h);
+	}
+}
+
+// Hierarchical Inheritance -> Shape class is inherited by Triangle and Circle class
+class Circle extends Shape {
+	public void area(int r) {
+		System.out.println((3.14)*r*r);
+	}
+}
+
+// Hybrid Inheritance -> Mixture of all the the above types of Inheritance
+
+// Multiple Inheritance -> Not directly possible in Java, possible through Interfaces
+
+public class Inheritance {
+	public static void main (String[] args) {
+		Triangle t1 = new Triangle();
+		t1.color = "red";
+	}
+}
---
+++ 03. Object Oriented Programming/OOPS.java
@@ -0,0 +1,86 @@
+class Pen {
+	
+	// defining properties of a Pen
+	String color;
+	String type; // ballpoint; gel
+	
+	// defining methods that a Pen object can perform
+	public void write() {
+		System.out.println("writing something");
+	}
+	
+	public void printColor() {
+		System.out.println(this.color); // this refers to the current object
+	}
+}
+
+class Student {
+	
+	String name;
+	int age;
+	
+	// non parameterized constructor
+	Student() {
+		System.out.println("constructor called");
+	}
+	
+	// parameterized constructor
+	Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	// copy constructor (copying info of object passed to current object)
+	Student(Student s2) {
+		this.name = s2.name;
+		this.age = s2.age;
+	}
+	
+	public void printInfo() {
+		System.out.println(this.name);
+		System.out.println(this.age);
+	}
+	
+	// this is known as function overloading(when functions have same name in same class) -> compile time polymorphism
+	public void printInfo(String name) {
+		System.out.println(name);
+	}
+	
+	public void printInfo(int age) {
+		System.out.println(age);
+	}
+	
+	public void printInfo(String name, int age) {
+		System.out.println(name);
+		System.out.println(age);
+	}
+}
+
+public class OOPS {
+	public static void main (String[] args) {
+		Pen pen1 = new Pen();
+		pen1.color = "blue";
+		pen1.type = "gel";
+		
+		pen1.write(); // writing something
+		
+		Pen pen2 = new Pen();
+		pen2.color = "black";
+		pen2.type = "ballpoint";
+		
+		pen1.printColor(); // blue
+		pen2.printColor(); // black
+		
+		//Student s1 = new Student();
+		//s1.name = "aman";
+		//s1.age = 24;
+		
+		//s1.printInfo();
+		
+		Student s2 = new Student("Shraddha", 22);
+		s2.printInfo();
+		
+		Student s3 = new Student(s2);
+		s2.printInfo();
+	}
+}
